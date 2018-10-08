729a9f2adf98dddfcb75f273ec32741217bd5dae
Eric_B <eric.bertaut@outlook.fr>
2018-10-08 14:51:40 +0400

    add MostPopularTest

---
+++ app/src/androidTest/java/com/eric_b/mynews/ReceivingNewsTest.java
@@ -0,0 +1,50 @@
+package com.eric_b.mynews;
+
+import android.util.Log;
+
+import com.eric_b.mynews.models.MostPopularPojo;
+import com.eric_b.mynews.models.TopStoriePojo;
+import com.eric_b.mynews.utils.TimesStream;
+import org.junit.Test;
+import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ReceivingNewsTest {
+
+    @Test
+    public void streamFetchTopStoriesTest() throws Exception {
+        //1 - Get the stream
+        Observable<TopStoriePojo> observableNews = TimesStream.streamFetchTopStorieNews("home");
+        //2 - Create a new TestObserver
+        TestObserver<TopStoriePojo> testObserver = new TestObserver<>();
+        //3 - Launch observable
+        observableNews.subscribeWith(testObserver)
+                .assertNoErrors()
+                .assertNoTimeout()
+                .awaitTerminalEvent();
+
+        TopStoriePojo newsFetched = testObserver.values().get(0);
+        assertThat("Top Storie receive news.",newsFetched.getNumResults() > 0);
+
+    }
+
+    @Test
+    public void streamFetchMostPopularTest() throws Exception {
+        //1 - Get the stream
+        Observable<MostPopularPojo> observableNews = TimesStream.streamFetchMostPopularNews();
+        //2 - Create a new TestObserver
+        TestObserver<MostPopularPojo> testObserver = new TestObserver<>();
+        //3 - Launch observable
+        observableNews.subscribeWith(testObserver)
+                .assertNoErrors()
+                .assertNoTimeout()
+                .awaitTerminalEvent();
+
+        MostPopularPojo newsFetched = testObserver.values().get(0);
+        assertThat("Most Popular receive news.",newsFetched.getNumResults() > 0);
+
+    }
+
+
+}
