86f6e15818fe603bfa69f6705b58edfaad0887ff
Eric_B <eric.bertaut@outlook.fr>
2018-10-09 14:22:48 +0400

    Add corrections in TopStoriesFragment

--- app/src/main/java/com/eric_b/mynews/utils/TopStorieService.java
+++ app/src/main/java/com/eric_b/mynews/utils/TopStorieService.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
-public interface NyTimesService {
+public interface TopStorieService {
 
     @GET("svc/topstories/v2/{category}.json?api-key=571bed2a4b0d429380eca6b006c553d3")
     Observable<TopStoriePojo> getNews(@Path("category") String category);
